27acbfcad96f6db93103e8870a606cb0e1c4d3e7
romistaro <romit.bali@gmail.com>
2024-01-13 23:38:06 -0500

    good

--- rec.py
+++ rec.py
@@ -9,7 +9,7 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 4096  # Buffer size
-RECORD_SECONDS = 7
+RECORD_SECONDS = 5
 FILENAMES = ['f1.wav', 'f2.wav']
 
 audio = pyaudio.PyAudio()
